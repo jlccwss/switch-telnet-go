c566e7a3deee25c6ce9cb2960191c8da0a4154be
Wison <jlccwss@163.com>
2021-08-08 21:02:41 +0800

    init

---
+++ telnet.go
@@ -0,0 +1,177 @@
+package telnet
+
+import (
+	"bufio"
+	"errors"
+	"net"
+	"strings"
+	"time"
+)
+
+const (
+	TIME_DELAY_AFTER_WRITE = 10
+)
+
+type Client struct {
+	Address string
+	Conn    net.Conn
+	buf     [4096]byte
+}
+
+func (c *Client) Write(conn net.Conn, bufs []byte) (n int, err error) {
+	n, err = conn.Write(bufs)
+	if err != nil {
+		return n, err
+	}
+	time.Sleep(time.Millisecond * TIME_DELAY_AFTER_WRITE)
+	return n, err
+}
+
+func (c *Client) Connect(address string) (err error) {
+	c.Conn, err = net.DialTimeout("tcp", address, 1*time.Second)
+	if err != nil {
+		return err
+	}
+	c.Conn.SetDeadline(time.Now().Add(3 * time.Second))
+
+	_, err = c.Conn.Read(c.buf[0:])
+	if err != nil {
+		return err
+	}
+
+	_, err = c.Conn.Read(c.buf[0:])
+	if err != nil {
+		return err
+	}
+
+	c.Conn.SetDeadline(time.Now().Add(15 * time.Second))
+
+	return err
+}
+
+func (c *Client) Login(username string, password string, enable string) error {
+login:
+	n, err := c.Write(c.Conn, []byte(username+"\n"))
+	if err != nil {
+		return err
+	}
+
+	n, err = c.Conn.Read(c.buf[0:])
+	if err != nil {
+		return err
+	}
+
+	n, err = c.Write(c.Conn, []byte(password+"\n"))
+	if err != nil {
+		return err
+	}
+
+	testCase := 0
+
+	for {
+		if testCase > 2 {
+			return errors.New("error login")
+		}
+		testCase++
+		n, err = c.Write(c.Conn, []byte("\n"))
+		if err != nil {
+			return err
+		}
+		n, err = c.Write(c.Conn, []byte("\n"))
+		if err != nil {
+			return err
+		}
+		n, err = c.Write(c.Conn, []byte("\n"))
+		if err != nil {
+			return err
+		}
+		n, err = c.Conn.Read(c.buf[0:])
+		if err != nil {
+			return err
+		}
+
+		if strings.HasSuffix(string(c.buf[0:n]), ">") {
+			break
+		}
+		if strings.HasSuffix(string(c.buf[0:n]), "Username:") {
+			goto login
+			break
+		}
+		if strings.HasSuffix(string(c.buf[0:n]), "Password:") {
+			n, err = c.Write(c.Conn, []byte(password+"\n"))
+			if err != nil {
+				return err
+			}
+		}
+	}
+	n, err = c.Write(c.Conn, []byte("enable\n"))
+	if err != nil {
+		return err
+	}
+
+	n, err = c.Conn.Read(c.buf[0:])
+	if err != nil {
+		return err
+	}
+	//fmt.Println(string(buf[0:n]))
+
+	n, err = c.Write(c.Conn, []byte(enable+"\n"))
+	if err != nil {
+		return err
+	}
+
+	n, err = c.Conn.Read(c.buf[0:])
+	if err != nil {
+		return err
+	}
+	//fmt.Println(string(buf[0:n]))
+
+	n, err = c.Write(c.Conn, []byte("terminal length 0\n"))
+	if err != nil {
+		return err
+	}
+
+	n, err = c.Conn.Read(c.buf[0:])
+	if err != nil {
+		return err
+	}
+	//fmt.Println(string(buf[0:n]))
+	return err
+}
+
+func (c *Client) Cmd(shell string) (context string, err error) {
+	_, err = c.Write(c.Conn, []byte(shell+"\n\n"))
+	if err != nil {
+		return "", err
+	}
+	//
+	//for {
+	//	n, err = conn.Read(buf[0:])
+	//	if err != nil {
+	//		return "",err
+	//	}
+	//	context += string(buf[0:n])
+	//	if strings.HasSuffix(string(buf[0:n]), "#") {
+	//		break
+	//	}
+	//}
+
+	reader := bufio.NewReader(c.Conn)
+
+	if reader == nil {
+		return "", errors.New("Create reader failed.")
+	}
+
+	for {
+		n, err := reader.Read(c.buf[0:])
+		if err != nil {
+			return "", err
+		}
+		context += string(c.buf[0:n])
+		if strings.HasSuffix(string(c.buf[0:n]), "#") {
+			break
+		}
+	}
+
+	return context, err
+}
